3c0f8826cdd09474477d9771038c2cc92f313251
steguiosaur <91378710+steguiosaur@users.noreply.github.com>
2023-12-03 12:19:43 +0800

    feat: enable read *.rens and *.rn file fixes #1
    + Add accessible file contents dynamic array in fileread.h

---
+++ include/fileread.h
@@ -0,0 +1,14 @@
+// `fileread.h` - header file for reading contents of rens file
+//
+// `fileread.c` scans if file is the accepted extension file (*.rens || *.rn).
+// It also allocates memory for a dynamic array where the contents of the
+// file is stored.
+
+// access the file contents with 'array_textdata'
+extern char *array_textdata;
+
+// detect file extension (*.rens || *.rn) and store values to 'array_textdata'
+extern int getRensFileContents(const char *filename);
+
+// free allocated array_textdata memory
+extern void freeTextdata();
--- test/test.md
+++
@@ -1,3 +0,0 @@
-# `test/` directory
-
-Store test related files.
